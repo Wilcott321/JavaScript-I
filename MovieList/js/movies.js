afd224eda8efb6f392bc94b559e33c1c0b675b7c
SheThrives11 <ewilson110@gmail.com>
2016-11-12 20:50:58 -0600

    Added data to movies object
    - Includes title, release date, duration, actors, and rating
    - Test passed in console

--- MovieList/js/movies.js
+++ MovieList/js/movies.js
@@ -4,12 +4,39 @@ and featured actors are displayed */
 
 /* Movie list */
 var movies = {
-	title
+	'Dr. Strange': {
+		title: 'Dr. Strange',
+		releaseDate: '4 November 2016',
+		duration: '116 mins',
+		actors: ['Benedict Cumberbatch', 'David Ejiofor', 'Tilda Swanson'],
+		rating: 'PG-13'
+	},
+	'Arrival': {
+		title: 'Arrival',
+		releaseDate: '11 November 2016',
+		duration: '116 mins',
+		actors: ['Amy Adams', 'Jeremy Renner', 'Forest Whitaker'],
+		rating: 'PG-13'
+	},
+	'Trolls': {
+		title: 'Trolls',
+		releaseDate: '4 November 2016',
+		duration: '92 mins',
+		actors: ['Anna Kendrick', 'Justin Timberlake', 'Zooey Deschanel'],
+		rating: 'PG'
+	},
+	'Almost Christmas': {
+		title: 'Almost Christmas',
+		releaseDate: '11 November 2016',
+		duration: '112 mins',
+		actors: ['Kimberly Elise', 'Danny Glover', 'Omar Epps'],
+		rating: 'PG-13'
+	}
 }
 
-/* Tests
 
-var movie = movies["index title"]
+
+var movie = movies["Almost Christmas"];
 console.log(movie.title);
 console.log(movie.releaseDate);
 console.log(movie.duration);
@@ -17,7 +44,5 @@ for (i in movie.actors) {
 	console.log(movie.actors[i]);
 }
 console.log(movie.rating);
-
-*/
 
 /* Click function for displaying info */
