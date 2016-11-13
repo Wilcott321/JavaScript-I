ca35062d1041a4f6bf77eaa0ced7cd1cbffc2daf
SheThrives11 <ewilson110@gmail.com>
2016-11-12 20:01:35 -0600

    Added comments
    Structures code and gives directions

---
+++ MovieList/js/movies.js
@@ -0,0 +1,7 @@
+/* Movie list displays information about the top movies released this
+weekend. When movie title is clicked, movie title, release date, duration,
+and featured actors are displayed */
+
+/* Movie list */
+
+/* Click function for displaying info */
