a0a6cbc6605b577a4bb152fa62a71d556f2b8439
SheThrives11 <ewilson110@gmail.com>
2016-11-12 20:37:45 -0600

    tests: Add tests
    The program should display movie title, release date, duration, actors,
    and rating in the console.log
    - Replace 'index title' with movie title
    - Run in console.log tests

--- MovieList/js/movies.js
+++ MovieList/js/movies.js
@@ -3,5 +3,21 @@ weekend. When movie title is clicked, movie title, release date, duration,
 and featured actors are displayed */
 
 /* Movie list */
+var movies = {
+	title
+}
+
+/* Tests
+
+var movie = movies["index title"]
+console.log(movie.title);
+console.log(movie.releaseDate);
+console.log(movie.duration);
+for (i in movie.actors) {
+	console.log(movie.actors[i]);
+}
+console.log(movie.rating);
+
+*/
 
 /* Click function for displaying info */
