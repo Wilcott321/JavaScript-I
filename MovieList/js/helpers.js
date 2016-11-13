91a81a5a992843aa71c015964506a05792832ac8
SheThrives11 <ewilson110@gmail.com>
2016-11-12 20:35:01 -0600

    Added coments to file for directions

---
+++ MovieList/js/helpers.js
@@ -0,0 +1,4 @@
+/* Contains the HTML strings that will be displayed when
+click functions are called from the movies.js file.
+The strings will be dispayed to the user when a movie title
+is clicked on */
