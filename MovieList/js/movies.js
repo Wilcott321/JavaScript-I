ddcba041dcee388ac38970ecfecee03619a6b38a
SheThrives11 <ewilson110@gmail.com>
2016-12-22 23:40:52 -0600

    refactor: Rewrote the prototype object
    - A better way to define the movie object using 'this' keyword

--- MovieList/js/movies.js
+++ MovieList/js/movies.js
@@ -2,61 +2,13 @@
 weekend. When movie title is clicked, movie title, release date, duration,
 and featured actors are displayed */
 
-/* Movie list */
-var movies = {
-	'Dr. Strange': {
-		title: 'Dr. Strange',
-		releaseDate: '4 November 2016',
-		duration: '116 mins',
-		actors: ['Benedict Cumberbatch', 'David Ejiofor', 'Tilda Swanson'],
-		rating: 'PG-13'
-	},
-	'Arrival': {
-		title: 'Arrival',
-		releaseDate: '11 November 2016',
-		duration: '116 mins',
-		actors: ['Amy Adams', 'Jeremy Renner', 'Forest Whitaker'],
-		rating: 'PG-13'
-	},
-	'Trolls': {
-		title: 'Trolls',
-		releaseDate: '4 November 2016',
-		duration: '92 mins',
-		actors: ['Anna Kendrick', 'Justin Timberlake', 'Zooey Deschanel'],
-		rating: 'PG'
-	},
-	'Almost Christmas': {
-		title: 'Almost Christmas',
-		releaseDate: '11 November 2016',
-		duration: '112 mins',
-		actors: ['Kimberly Elise', 'Danny Glover', 'Omar Epps'],
-		rating: 'PG-13'
-	}
+/* Movie list protoype function */
+function movie(title, duration, rating, starring, gross) {
+	this.title = title;
+  	this.duration = duration;
+  	this.rating = rating;
+  	this.starring = starring;
+  	this.gross = gross;
 }
 
-/* Define global variables */
-var boxOfficePickOne = movies["Dr. Strange"];
-var boxOfficePickTwo = movies["Arrival"];
-var boxOfficePickThree = movies["Trolls"];
-var boxOfficePickFour = movies["Almost Christmas"];
-
-/* Click function for displaying info */
-
-
-/* Test
-
-var movie = movies["Almost Christmas"];
-console.log(movie.title);
-console.log(movie.releaseDate);
-console.log(movie.duration);
-for (i in movie.actors) {
-	console.log(movie.actors[i]);
-}
-console.log(movie.rating);
-
-function changeNow() {
-	document.getElementById("movie-content").innerHTML = "test";
-}
-
-*/
 
