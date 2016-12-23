6d06d2ad6b4dd7e163d5910b6b4005269eb289f0
SheThrives11 <ewilson110@gmail.com>
2016-12-22 22:51:36 -0600

    Removed helpers.js file
    - It's not needed

--- MovieList/js/helpers.js
+++
@@ -1,6 +0,0 @@
-/* Contains the HTML strings that will be displayed when
-click functions are called from the movies.js file.
-The strings will be dispayed to the user when a movie title
-is clicked on */
-
-/* Movie data displayed as list items*/
